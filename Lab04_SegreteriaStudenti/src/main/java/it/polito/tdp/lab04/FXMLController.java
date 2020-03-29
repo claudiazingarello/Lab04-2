31cd96d23a53fdf157b2fa3a5b2720cee75d0f14
claudiazingarello <s238072@studenti.polito.it>
2020-03-29 20:42:15 +0200

    Completato EntryPoint e iniziato FXMLController

--- Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
+++ Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
@@ -1,23 +1,111 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.lab04;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.lab04.model.Corso;
+import it.polito.tdp.lab04.model.Model;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class FXMLController {
+	
+	Model model;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private ChoiceBox<Corso> choiceBox;
+
+    @FXML
+    private Button btnCercaIscrittiCorso;
+
+    @FXML
+    private TextField txtMatricola;
+
+    @FXML
+    private CheckBox checkBox;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtCognome;
+
+    @FXML
+    private Button btnCercaCorsi;
+
+    @FXML
+    private Button btnIscrivi;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void cercaCorsi(ActionEvent event) {
 
     }
+
+    @FXML
+    void cercaIscrittiCorso(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCheck(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doIscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void scegliCorso(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert choiceBox != null : "fx:id=\"choiceBox\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaIscrittiCorso != null : "fx:id=\"btnCercaIscrittiCorso\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert checkBox != null : "fx:id=\"checkBox\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaCorsi != null : "fx:id=\"btnCercaCorsi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model=model;
+		
+		//Richiamo metodo dal model che recupera i corsi, salvandoli in una lista
+		
+		//Ordino la lista alfabeticamente
+		
+		//popolo la choiseBox con la lista appena ottenuta
+	}
 }
